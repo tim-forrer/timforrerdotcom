b2a1aeb157320ddcf8f9fe9b3446572f1d9ba818
tim@mbp <timforrer16@gmail.com>
2026-06-10 23:22:39 +0900

    chore: scaffold Astro project with Cloudflare adapter

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
+
+export default defineConfig({
+  output: 'static',
+  adapter: cloudflare(),
+  site: 'https://timforrer.com',
+});
